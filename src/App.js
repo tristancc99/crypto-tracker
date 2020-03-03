25930e9554ffb4ed724fa467dea452e4c353a64a
Judas <pleasedontforgetme99@gmail.com>
2020-03-02 21:00:05 -0500

    init app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.querySelector("#root");
+
+const App = () => {
+  return <div> App Boilerplate </div>;
+};
+
+export default App;
