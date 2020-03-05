3399c562dfcaac1bad466c7281d75e81220a4ad5
Judas <pleasedontforgetme99@gmail.com>
2020-03-04 19:36:06 -0500

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
-import Pagination from "react-bootstrap/Pagination"
-import Button from "react-bootstrap/Button"
-import ListGroup from "react-bootstrap/ListGroup"
-import Sidebar from "react-sidebar"
-import { useForm } from "react-hook-form"
-import CoinCard from "./components/CoinCard"
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Pagination from "react-bootstrap/Pagination";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import Sidebar from "react-sidebar";
+import { useForm } from "react-hook-form";
+import CoinCard from "./components/CoinCard";
 
-const root = document.querySelector("#root")
+const root = document.querySelector("#root");
 
 const App = () => {
-  const { register, handleSubmit, errors } = useForm()
+  const { register, handleSubmit, errors } = useForm();
 
-  let [coins, setCoins] = useState()
-  let [sidebarOpen, setSidebarOpen] = useState(true)
-  let [tracking, setTracking] = useState([])
+  let [coins, setCoins] = useState();
+  let [sidebarOpen, setSidebarOpen] = useState(true);
+  let [tracking, setTracking] = useState([]);
 
-  const url = "https://api.coinranking.com/v1/public/coins"
+  const url = "https://api.coinranking.com/v1/public/coins";
 
   useEffect(() => {
     axios.get(url).then(function(response) {
-      setCoins(response.data.data.coins)
-    })
-  }, [])
+      setCoins(response.data.data.coins);
+    });
+  }, []);
 
   const onSubmit = data => {
     axios
       .get(
         `https://api.coinranking.com/v1/public/coins?prefix=${data.searchCoins}`
       )
-      .then(response => setCoins(response.data.data.coins))
-  }
+      .then(response => setCoins(response.data.data.coins));
+  };
 
   function trackCoin(e) {
-    console.log(e.target.getAttribute("data-id"))
-    let coinID = e.target.getAttribute("data-id")
+    console.log(e.target.getAttribute("data-id"));
+    let coinID = e.target.getAttribute("data-id");
     axios
       .get(`https://api.coinranking.com/v1/public/coin/${coinID}`)
       .then(function(response) {
-        console.log(response)
-        setTracking([...tracking, response.data.data.coin])
-      })
+        console.log(response);
+        setTracking([...tracking, response.data.data.coin]);
+      });
   }
 
-  console.log(coins, tracking)
+  console.log(coins, tracking);
 
   return (
     <div>
@@ -87,7 +87,7 @@ const App = () => {
         style={{ sidebar: { background: "grey" } }}
       >
         <div className="content">
-          <h1>Crypto Tracker</h1>
+          <h1>&#x1F50D; Crypto Tracker</h1>
           <hr />
           <div className="trackingList">
             {tracking && tracking.map(trackee => <CoinCard data={trackee} />)}
@@ -95,7 +95,7 @@ const App = () => {
         </div>
       </Sidebar>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
